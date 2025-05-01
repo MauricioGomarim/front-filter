06f67621ebbfc024548808fbff69cc90b478aa82
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2025-05-01 17:41:45 -0300

    service

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,6 +2,5 @@ import axios from "axios"
 
 export const api = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://backend-filtro-an7.koyeb.app",
-  withCredentials: true
+  baseURL: "https://backend-filtro-an7.koyeb.app"
 })
